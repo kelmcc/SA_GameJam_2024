09fbaa820a335859be3a4c6014d45c7df3af8a64
KellyMAM <kelmccarter@gmail.com>
2024-10-21 14:10:27 +0200

    wip

--- Assets/Scripts/StartScreen/StartGameClicker.cs
+++ Assets/Scripts/StartScreen/StartGameClicker.cs
@@ -8,14 +8,30 @@ namespace UI
     public class StartGameClicker : MonoBehaviour
     {
         [SerializeField] private Button _startButton;
+        [SerializeField] private List<GameObject> _slides = new List<GameObject>();
+        [SerializeField] private int _currentIndex;
 
         private void Awake()
         {
+            ResetSlides();
+            
             _startButton.onClick.AddListener(() =>
             {
+                if (_slides)
+                {
+                    
+                }
                 TimeTicker.Instance.transform.parent = transform;
                 SceneManager.LoadScene(1);
             });
+        }
+        
+        private void ResetSlides()
+        {
+            foreach (var slide in _slides)
+            {
+                slide.SetActive(false);
+            }
         }
     }
 }
